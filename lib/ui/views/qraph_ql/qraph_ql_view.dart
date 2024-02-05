61390d0cd1101e69ada4760209dfac3fe56f9f3e
Kevin Langat <kevin.langat@nathandigital.com>
2024-02-05 12:23:32 +0300

    create a graph ql page and fetch data using graphql

--- lib/ui/views/qraph_ql/qraph_ql_view.dart
+++ lib/ui/views/qraph_ql/qraph_ql_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_stacked/ui/common/ui_helpers.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:sizer/sizer.dart';
 import 'package:stacked/stacked.dart';
 
 import 'qraph_ql_viewmodel.dart';
@@ -12,11 +15,56 @@ class QraphQlView extends StackedView<QraphQlViewModel> {
     QraphQlViewModel viewModel,
     Widget? child,
   ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+    return Query(
+      options: QueryOptions(
+        document: gql(viewModel.spaceXShips),
       ),
+      builder: (QueryResult result,
+          {VoidCallback? refetch, FetchMore? fetchMore}) {
+        if (result.hasException) {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                result.exception.toString(),
+              ),
+            ),
+          );
+        }
+
+        if (result.isLoading) {
+          return const Text('Loading');
+        }
+
+        // final dragons = result.data!['dragons'] as List<dynamic>;
+        final ships = result.data!['ships'] as List<dynamic>;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('QraphQL Example'),
+            elevation: 0,
+          ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                verticalSpaceSmall,
+                verticalSpaceSmall,
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: ships.length,
+                    itemBuilder: (context, index) {
+                      final ship = ships[index];
+                      return ListTile(
+                        title: Text(ship['name']),
+                        subtitle: Text("${ship['type']}"),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
--- lib/ui/views/qraph_ql/qraph_ql_viewmodel.dart
+++ lib/ui/views/qraph_ql/qraph_ql_viewmodel.dart
@@ -1,3 +1,62 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:stacked/stacked.dart';
 
-class QraphQlViewModel extends BaseViewModel {}
+class QraphQlViewModel extends BaseViewModel {
+  final String _query = """
+    query users {
+      users {
+        id
+        name
+        rocket
+        twitter
+      }
+    }
+  """;
+
+  String get query => _query;
+
+  final String _jobsQuery = '''
+    query {
+      jobs {
+        id
+        title
+        company {
+          name
+        }
+      }
+    }
+''';
+
+  String get jobsQuery => _jobsQuery;
+
+  final String _spaceXDragons = """
+query Dragons {
+  dragons {
+    name
+    first_flight
+    diameter {
+      feet
+    }
+    launch_payload_mass {
+      lb
+    }
+  }
+}
+""";
+
+  String get spaceXDragons => _spaceXDragons;
+
+  final String _spaceXShips = """
+query Ships {
+  ships {
+    id
+    model
+    name
+    type
+    status
+  }
+}
+""";
+
+  String get spaceXShips => _spaceXShips;
+}
